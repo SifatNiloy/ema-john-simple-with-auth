cfb44c182147da8c4137a0677eb7ce08637b4a17
unknown <sifatniloy18@gmail.com>
2022-10-17 20:55:13 -0600

    pagination added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,7 +9,16 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useProducts();
     const [cart, setCart] = useState([]);
-
+    const [pageCount, setPageCount] = useState(0);
+    useEffect(() => {
+        fetch('http://localhost:5000/productCount')
+            .then(res => res.json())
+            .then(data => {
+                const count = data.count;
+                const pages = Math.ceil(count / 10);
+                setPageCount(pages);
+            })
+    }, [])
     useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
@@ -52,7 +61,14 @@ const Shop = () => {
                         handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
+                <div className='pagination'>
+                    {
+                        [...Array(pageCount).keys()].
+                            map(number => <button>{number + 1}</button>)
+                    }
+                </div>
             </div>
+
             <div className="cart-container">
                 <Cart cart={cart}>
                     <Link to="/orders">
